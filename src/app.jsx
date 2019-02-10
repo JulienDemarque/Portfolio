ff80f592da764e373d07a623b6690bdb7d47cd0d
julien demarque <julien_demarque@hotmail.fr>
2019-02-10 13:19:03 +0200

    added loading screen

--- src/app.jsx
+++ src/app.jsx
@@ -4,9 +4,30 @@ import { BrowserRouter } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import "./styles/styles.scss";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Portfolio />
-  </BrowserRouter>,
-  document.getElementById("app")
-);
+class App extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    // the setTimeout just simulates an async action, after which the component will render the content
+    setTimeout(() => this.setState({ loading: false }), 1000);
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      // if your component doesn't have to wait for an async action, remove this block
+      return null; // render null when app is not ready
+    }
+
+    return (
+      <BrowserRouter>
+        <Portfolio />
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
